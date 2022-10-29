cdfca7be308b04fc041b8314e1cdbbfd98f37744
sam price <sams-mbp@sams-MBP.lan>
2022-10-29 16:36:42 -0500

    Added the ability to append items to containers in the hierarchy

--- src/main/java/uab/bogra/farmdashboard/Item.java
+++ src/main/java/uab/bogra/farmdashboard/Item.java
@@ -1,11 +1,7 @@
 package uab.bogra.farmdashboard;
 
-import javafx.scene.image.Image;
-
-public class Item extends Container{
+public class Item extends Container {
     public Item(String name) {
         super(name);
     }
-
-    Image file = new Image("resources/icons/file.png");
 }
