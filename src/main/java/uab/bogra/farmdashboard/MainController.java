8ae5b4bf9575d6db1c377fd41260ac5276dd544a
ethanbrucee <ebruce@uab.edu>
2022-10-30 12:42:38 -0500

    adding change item location/dimensions

--- src/main/java/uab/bogra/farmdashboard/MainController.java
+++ src/main/java/uab/bogra/farmdashboard/MainController.java
@@ -263,6 +263,7 @@ public class MainController implements Initializable {
             }
             shapesPane.getChildren().clear();
             drawContainers();
+            drawItemShapes();
         });
     }
 
@@ -334,6 +335,7 @@ public class MainController implements Initializable {
             }
             shapesPane.getChildren().clear();
             drawContainers();
+            drawItemShapes();
         });
     }
 
