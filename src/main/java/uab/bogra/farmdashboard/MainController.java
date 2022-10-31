618baab19bf671a9aa3e2f1a1d15ba30065cf38a
williamkylewinn <55703400+williamkylewinn@users.noreply.github.com>
2022-10-30 20:42:03 -0500

    Add files via upload
    adding gohome function for drone

--- src/main/java/uab/bogra/farmdashboard/MainController.java
+++ src/main/java/uab/bogra/farmdashboard/MainController.java
@@ -470,4 +470,9 @@ public class MainController implements Initializable {
         Square.toFront();
         Square.coverFarm();
     }
+
+    public void goHome(){
+        Square.toFront();
+        Square.goHome();
+    }
 }
